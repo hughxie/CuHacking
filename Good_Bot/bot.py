b6deea585ea017955f913559482dd8277b09e792
AngelOnFira <wizcardforest@gmail.com>
2017-03-05 00:28:30 -0500

    Gameplay

--- Good_Bot/bot.py
+++ Good_Bot/bot.py
@@ -14,9 +14,13 @@ state = "lobby"
 UserWaiting = ""
 server = []
 game = []
+gameTeam = []
+playersSubmitted = []
 team1 = []
 team2 = []
+questionsAsked = 0
 answer = -1
+trueCount = falseCount = 0
 
 questions = [[0, "Richard is Cool"], [1, "Forest > Aidan"]]
 
@@ -113,7 +117,41 @@ while True:
                                 Send_whisper(str(team2[p]) + ", you're on team 2 with " + str(len(team2) - 1) + " other players", str(team2[p]))
 
                     if state == "game":
-                        print "ingame"
+                        #Check if the user is part of this current game, and if they have not answered yet
+                        if username in gameTeam and username not in playerSubmitted:
+                            #if they answered true
+                            if (message == "true"):
+                                #add 1 to the tally of people who have chosen
+                                trueCount++
+                                #add the player to the list of players who have voted
+                                playerSubmitted.append(username)
+                                
+                            elif (message == "false"):
+                                falseCount++
+                                playerSubmitted.append(username)
+
+                        #if the list of players who have voted is equal to the list of players playing
+                        if len(playerSubmitted) and len(gameTeam):
+                            #find out how many people choose the right answer
+                            if (answer = 0):
+                                rightAnswerCount = falseCount
+                                wrongAnswerCount = trueCount
+                            elif (answer = 1):
+                                rightAnswerCount = trueCount
+                                wrongAnswerCount = falseCount
+
+                            #display how many people got it right and wrong    
+                            Send_message(str(rightAnswerCount) + " people got that question right, " + str(wrongAnswerCount) + " people got it wrong.")
+
+                            #reset all of the numbers
+                            rightAnswerCount = wrongAnswerCount = trueCount = falseCount = 0
+
+                            #you need to set the playerSubmitted list back to empty
+
+                            #make another question
+                            answer = askQuestion(questions)
+
+                            #here we need to check that we still need to ask questions rather than just returning to the lobby
 
                 if MODT:
                     #print (username + ": " + message)
