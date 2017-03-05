7d174236d096b02849b8272d197094172bfced87
AngelOnFira <wizcardforest@gmail.com>
2017-03-05 11:38:07 -0500

    still doing class

--- Good_Bot/bot.py
+++ Good_Bot/bot.py
@@ -108,12 +108,12 @@ while True:
                             state = "game"
                             print team1
                             print team2
-                            #Ask a question
-                        elif message == "!start" and mode == "":
-                            Send_message(s, username + " , the mode has not been set yet! Enter !mode *mode name* to set it.")
 
                             if (mode == "trivia"):
                                 gameClass.setupTfTrivia(gameTeam, s)
+                            #Ask a question
+                        elif message == "!start" and mode == "":
+                            Send_message(s, username + " , the mode has not been set yet! Enter !mode *mode name* to set it.")
 
                     #run the game. problem with this, not able to call function
                     if state == "game":
